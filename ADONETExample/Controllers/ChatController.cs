e9dfba0afcc154658ba59f2568e358b8f95fa566
zh-987 <zhasulanasainov@gmail.com>
2022-11-02 22:00:55 +0600

    Adding Chat by Ajax

---
+++ ADONETExample/Controllers/ChatController.cs
@@ -0,0 +1,94 @@
+﻿using ADONETExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADONETExample.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        static Chat chatmodel;
+        public ActionResult Index(string User, bool? LogOn, bool? LogOff, string ChatMessage)
+        {
+            try
+            {
+                if(chatmodel == null)
+                {
+                    chatmodel = new Chat();
+                }
+                if (!Request.IsAjaxRequest())
+                {
+                    return View();
+                }
+                else if (LogOn != null && (bool)LogOn)
+                {
+                    if(chatmodel.User.FirstOrDefault(x =>x.Name == User) != null)
+                    {
+                        throw new Exception("Пользователь таким именем уже существует ... ");
+                    }
+                    else if(chatmodel.User.Count > 3)
+                    {
+                        throw new Exception("Чат переполнено");
+                    }
+                    else
+                    {
+                        chatmodel.User.Add(new Chat.ChatUsers()
+                        {
+                            Name = User,
+                            Login = DateTime.Now,
+                            LastVisit = DateTime.Now
+                        });
+
+                        chatmodel.Messages.Add(new Chat.ChatMessage()
+                        {
+                            Text = User + "вошел в чат",
+                            Date = DateTime.Now
+                        });
+                    }
+                    return PartialView("ChatRoom",chatmodel);
+                }
+                else if(LogOff != null && (bool)LogOff)
+                {
+                    LogOffChat(chatmodel.User.FirstOrDefault(x => x.Name == User));
+                    return PartialView("ChatRoom", chatmodel);
+                }
+                else
+                {
+                    Chat.ChatUsers currentUser = chatmodel.User.FirstOrDefault(x => x.Name == User);
+
+                    currentUser.LastVisit = DateTime.Now;
+
+                    if (!string.IsNullOrEmpty(ChatMessage))
+                    {
+                        chatmodel.Messages.Add(new Chat.ChatMessage()
+                        {
+                            users = currentUser,
+                            Text = ChatMessage,
+                            Date = DateTime.Now
+                        });
+                    }
+                    return PartialView("MessageHistory",chatmodel);
+                }
+            }
+            catch(Exception ex) {
+                Response.StatusCode = 400;
+                return Content(ex.Message);
+            }
+        }
+
+        public void LogOffChat(Chat.ChatUsers user)
+        {
+            chatmodel.User.Remove(user);
+
+            chatmodel.Messages.Add(new Chat.ChatMessage()
+            {
+                Text = user + "Покинул чат",
+                Date = DateTime.Now
+            }
+                );
+        }
+    }
+}
---
+++ ADONETExample/Models/Chat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADONETExample.Models
+{
+    public class Chat
+    {
+        public List<ChatUsers> User;
+
+        public List<ChatMessage> Messages; 
+
+        public Chat()
+        {
+            User = new List<ChatUsers>();
+            Messages = new List<ChatMessage>();
+
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат запущено, время:" + DateTime.Now
+            }
+                );
+        }
+        public class ChatUsers
+        {
+            public string Name;
+            public DateTime Login;
+            public DateTime LastVisit;
+        }
+
+        public class ChatMessage
+        {
+            public ChatUsers users;
+            public DateTime Date = DateTime.Now;
+            public string Text;
+        }
+
+
+
+    }
+}
